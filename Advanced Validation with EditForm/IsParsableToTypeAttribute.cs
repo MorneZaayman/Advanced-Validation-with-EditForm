8746b8887b5238d62e8379afd2a356bff926b0b0
Morné Zaayman <mornezaayman@morzgroup.com>
2023-12-28 16:34:38 +0100

    Add project files.

---
+++ Advanced Validation with EditForm/IsParsableToTypeAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Advanced_Validation_with_EditForm
+{
+    public sealed class IsParsableToTypeAttribute<T>() : ValidationAttribute(DefaultErrorMessage) where T : IParsable<T>
+    {
+        private static readonly string DefaultErrorMessage = "The {0} field is not parsable to type '" + typeof(T).FullName + "'";
+
+        public override bool IsValid(object value)
+        {
+            return value is string valueAsString
+                   && T.TryParse(valueAsString, null, out _);
+        }
+    }
+}
